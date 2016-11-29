7d5f01b775e06bcf7cc9a13abd629acb4b33b934
Matthew Fritz <matthew.fritz@csun.edu>
2016-11-29 15:00:52 -0800

    Initial commit of composer-specific files and locked dependencies

---
+++ src/csunmetalab/ProxyPass/Providers/ProxyPassServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CSUNMetaLab\ProxyPass\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+use URL;
+
+class ProxyPassServiceProvider extends ServiceProvider
+{
+	public function register() {
+
+	}
+
+	public function boot() {
+
+	}
+}
